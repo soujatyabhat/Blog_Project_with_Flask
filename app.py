18970bea4cdc8ea0173bc0bc047540ebc7f3eff5
Soujatya Bhattacharya <65819358+soujatyabhat@users.noreply.github.com>
2021-03-28 00:32:08 +0530

    28th_commit

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -11,7 +10,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/blog'
 
 
+
 db = SQLAlchemy(app)
+
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = "candila.ppf105@gmail.com",
+    MAIL_PASSWORD = "@soujatya123**"
+)
+
+mail = Mail(app)
 
 class contacts(db.Model):
     u_sno = db.Column(db.Integer, primary_key=True)
@@ -20,12 +30,26 @@ class contacts(db.Model):
     u_number = db.Column(db.Integer, nullable=True)
     u_message = db.Column(db.String(1000), nullable=True)
     
+class posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    tagline = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    #img_file = db.Column(db.String(12), nullable=True)
+    
+class sign_in(db.Model):
+    email = db.Column(db.String(50), primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
     
 x = datetime.datetime.now()
+
     
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html',heading = "CodingThunder", heading_footer = "A tech blog", banner = "contact-bg.jpg",year = x.strftime("%Y"))
+    p = posts.query.order_by(posts.sno).all()[0:3]
+    return render_template('index.html',heading = "CodingThunder", heading_footer = "A tech blog", banner = "contact-bg.jpg",year = x.strftime("%Y"),posts = p)
 
 @app.route('/post', methods=["GET", "POST"])
 def post():
@@ -50,6 +74,32 @@ def save():
         entry = contacts(u_email = email, u_name = name, u_number = number, u_message = message)
         db.session.add(entry)
         db.session.commit()
+        mail.send_message('New message from ' + str(name),
+                          sender = email,
+                          recipients = ["candila.ppf105@gmail.com"],
+                          body = message + "\n" + str(number)
+                          )
     return render_template('render.html', link = "/contact")
+
+@app.route("/login", methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route("/dashboard", methods=['GET','POST'])
+def dashboard():
+        if(request.method == 'POST'):
+            email = request.form['email']
+            password  = request.form['password']
+            sign = sign_in.query.filter_by(email = email).first()
+            
+            if sign.email == email and sign.password == password:
+                return render_template('dashboard.html',status = "Connected!!")
+            else:
+                return render_template('dashboard.html',status = "Disconnected!!")
+
+@app.route("/post_details/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', post=post,banner = "post-bg.jpg", heading = post.title, heading_footer = post.tagline)
 
 app.run()
